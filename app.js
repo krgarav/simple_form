5d46a9eb8244462592070705706668c3c1d8b350
krgarav <krgarav@gmail.com>
2023-02-16 01:08:50 +0530

    added edit button

--- app.js
+++ app.js
@@ -21,19 +21,22 @@ const newPhone = deserialised.phone
 const newEmail=deserialised.email
 const items =document.getElementById("items")
 const newli = document.createElement("li")
+//create delete button
 const deleteBtn = document.createElement("input")
 deleteBtn.type="button"
 deleteBtn.value= "Delete"
 deleteBtn.setAttribute('onclick','deleteli(event)')
+//create edit button
+const editBtn = document.createElement("input")
+editBtn.type="button"
+editBtn.value ="edit"
+editBtn.setAttribute('onclick','editbtn(event)')
 
 const newtext = document.createTextNode(`name:${newName}-Email:${newEmail}-Phone No:${newPhone}`)
 newli.appendChild(newtext)
 newli.appendChild(deleteBtn)
+newli.appendChild(editBtn)
 items.appendChild(newli)
-
-
-
-
 }
 
 
@@ -44,4 +47,19 @@ function deleteli(event){
     console.log(key.length)
     localStorage.removeItem(key)
    }
+
+ function editbtn(event){
+
+    event.target.parentNode.style.display="none"
+    const liName=event.target.parentNode.textContent.split("-")[0].split(":")[1].toString()
+    const liEmail=event.target.parentNode.textContent.split("-")[1].split(":")[1].toString()
+    const liPhone = event.target.parentNode.textContent.split("-")[2].split(":")[1].toString()
+    console.log(liName)
+    console.log(liEmail)
+    console.log(liPhone)
+     document.getElementById("name").value=liName
+     document.getElementById("email").value = liEmail
+     document.getElementById("phone").value=liPhone
+    localStorage.removeItem(liEmail)
+ }  
  
