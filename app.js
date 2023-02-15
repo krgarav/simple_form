1c35e650c3ca997a95e246ff23f224f0bb385bba
krgarav <krgarav@gmail.com>
2023-02-16 00:45:38 +0530

    added delete functionality from localestorage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // const names = document.getElementById("name")
 // const email = document.getElementById("email")
 // const phone = document.getElementById("phone")
-let i=1
+
 function onsubmitform(event){
     const names = document.getElementById("name").value
 const email = document.getElementById("email").value
@@ -13,17 +13,35 @@ const obj= {
     "phone":phone
 }
 const serialised= JSON.stringify(obj)
-localStorage.setItem(`obj${i}`,serialised)
+localStorage.setItem(email,serialised)
 
-const deserialised=JSON.parse( localStorage.getItem(`obj${i}`))
+const deserialised=JSON.parse( localStorage.getItem(email))
 const newName = deserialised.names
 const newPhone = deserialised.phone
 const newEmail=deserialised.email
 const items =document.getElementById("items")
 const newli = document.createElement("li")
-const newtext = document.createTextNode(`name:${newName} - Email:${newEmail} - Phone No:${newPhone}`)
+const deleteBtn = document.createElement("input")
+deleteBtn.type="button"
+deleteBtn.value= "Delete"
+deleteBtn.setAttribute('onclick','deleteli(event)')
+
+const newtext = document.createTextNode(`name:${newName}-Email:${newEmail}-Phone No:${newPhone}`)
 newli.appendChild(newtext)
+newli.appendChild(deleteBtn)
 items.appendChild(newli)
-console.log(newli)
-i=i+1
+
+
+
+
 }
+
+
+function deleteli(event){
+    event.target.parentNode.style.display="none"
+    // localStorage.removeItem(event.target.parentNode)
+    const key=event.target.parentNode.textContent.split("-")[1].split(":")[1].toString()
+    console.log(key.length)
+    localStorage.removeItem(key)
+   }
+ 
